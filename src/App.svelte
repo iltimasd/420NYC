aefd3731e37878550a5fa9022ac8da28b940f132
Iltimas Doha <iltimasd@users.noreply.github.com>
2021-04-18 14:17:50 -0400

    credit myself <3

--- src/App.svelte
+++ src/App.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <Map />
+<a href="https://twitter.com/iltimasdoha">@iltimasdoha</a>
+
+<style>
+  a {
+    top: 0;
+    left: 0;
+    position: fixed;
+    color: forestgreen;
+  }
+</style>
